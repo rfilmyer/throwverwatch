10fe6c02b38c944e2a877abc50dfe6a72b4f13c8
Roger Filmyer <roger.filmyer@gmail.com>
2017-02-25 18:49:32 -0800

    added a lot of docstrings

--- __main__.py
+++ __main__.py
@@ -1,57 +1,102 @@
 import logging
 import argparse
 import requests
-#import keyboard
+import keyboard
 from bs4 import BeautifulSoup
 
 logger = logging.getLogger("throwverwatch")
 
 parser = argparse.ArgumentParser(description="Track Overwatch Stats")
-parser.add_argument("battletag", nargs="?", default="Calvin#1337", help="Your Battle.net username in the format 'Calvin#1337'")
-parser.add_argument("--region", dest="region", default="us", help="Overwatch Region (us, kr, eu)")
-parser.add_argument("--platform", dest="platform", default="pc", help="Platform (pc, ps4, xbone)")
+parser.add_argument("battletag", nargs="?", default="Calvin#1337",
+                    help="Your Battle.net username in the format 'Calvin#1337'")
+parser.add_argument("--region", dest="region", default="us",
+                    help="Overwatch Region (us, kr, eu)")
+parser.add_argument("--platform", dest="platform", default="pc",
+                    help="Platform (pc, ps4, xbone)")
 args = parser.parse_args()
 
 HEROES = ["Genji", "McCree", "Pharah", "Reaper", "Soldier: 76", "Sombra", "Tracer", "Bastion", "Hanzo", "Junkrat",
           "Mei", "Torbjörn", "Widowmaker", "D.Va", "Reinhardt", "Roadhog", "Winston", "Zarya", "Ana", "Lúcio", "Mercy",
           "Symmetra", "Zenyatta"]
 
-def assemble_url(battletag, device='pc', region='us'):
+
+def assemble_url(battletag: str, device: str = 'pc', region: str = 'us') -> str:
+    """
+    Returns the URL to a user's stats page on PlayOverwatch.com
+    :rtype: str
+    :param battletag: A Battle.net battletag, including character code. (Ex: "Calvin#1337", "짬내#3905")
+    :param device: pc or any of the console platforms (I've only tested PC)
+    :param region: us, kr, or eu, corresponding to the battletag's account in the given region.
+    :return:
+    """
     username = battletag.replace('#', '-')
     return "https://playoverwatch.com/en-us/career/{device}/{region}/{username}".format(device=device, region=region, username=username)
 
 
-def get_page(battletag, device='pc', region='us', session=None):
-    logger.debug("Looking up player {player} on PlayOverwatch.com".format(player=battletag))
+def get_page(url: str, session: requests.Session = None) -> BeautifulSoup:
+    """
+    Grabs a webpage and parses it with BeautifulSoup
+    """
     session = session if session else requests
-    response = session.get(assemble_url(battletag, device='pc', region=region))
-    logger.info("Found page for player {player}".format(player=battletag))
+    response = session.get(url)
 
     return BeautifulSoup(response.text, 'html.parser')
 
 def find_stat_in_table(stat, div):
+    """
+    Table rows are formatted like so...
+    <tbody>
+        <tr>
+            <td>Melee Final Blows</td>
+            <td>167</td>
+        </tr>
+        <tr>
+            <td>Solo Kills</td>
+            <td>5,124</td>
+        </tr>
+        <tr>
+            <td>Objective Kills</td>
+            <td>5,762</td>
+        </tr>
+    </tbody>
+
+    This is a convenient function to extract a statistic, given its name.
+
+    :param stat: The name of a statistic (Ex: "Melee Final Blows", "Medals - Gold")
+    :param div: could also be a tbody in the future
+    :return:
+    """
     for tr in div.find_all("tr"):
         if tr.td:
             if tr.td.text == stat:
                 return tr.find_all("td")[1].text
 
-def parse_stats_page(soup):
+
+def parse_stats_page(soup: BeautifulSoup) -> dict:
+    """
+    Extracts player statistics from a web page parsed by BeautifulSoup.
+    :param soup: A parsed copy of the PlayOverwatch.com stats page for a user.
+    :return: dict
+    """
     stats = {}
     stats["season_rank"] = soup.find(class_="competitive-rank").div.text
-    quick_play_stats = soup.find(id="quickplay").find(attrs={"data-category-id": "0x02E00000FFFFFFFF"}).find_all("div")
+
+    all_heroes_category_id = "0x02E00000FFFFFFFF"
+    quick_play_stats = soup.find(id="quickplay").find(attrs={"data-category-id": all_heroes_category_id}).find_all("div")
     quick_play_game_game_stats = next(div for div in quick_play_stats if div.table.thead.span.text == "Game")
     #stats["quick_play_games"] = find_stat_in_table("Games Played", quick_play_game_game_stats)
     stats["quick_play_wins"] = find_stat_in_table("Games Won", quick_play_game_game_stats)
 
-    competitive_stats = soup.find(id="competitive").find(attrs={"data-category-id": "0x02E00000FFFFFFFF"}).find_all("div")
+    competitive_stats = soup.find(id="competitive").find(attrs={"data-category-id": all_heroes_category_id}).find_all("div")
     competitive_game_stats = next(div for div in competitive_stats if div.table.thead.span.text == "Game")
     stats["competitive_games"] = find_stat_in_table("Games Played", competitive_game_stats)
     stats["competitive_wins"] = find_stat_in_table("Games Won", competitive_game_stats)
 
     # This is dead code for now, I'll add this when I add the other 6 metrics
     stats["heroes"] = {}
-    for hero_bar in soup.find(id="competitive").find(attrs={"data-category-id": "overwatch.guid.0x0860000000000021"}).find_all(
-        class_="bar-text"):
+    for hero_bar in soup.find(id="competitive").find(
+            attrs={"data-category-id": "overwatch.guid.0x0860000000000021"}).find_all(
+                class_="bar-text"):
         hero = hero_bar.find(class_="title").text
         unparsed_time = hero_bar.find(class_="description").text
         if unparsed_time == '--':
@@ -65,8 +110,29 @@ def parse_stats_page(soup):
         stats["heroes"][hero] = time
     return stats
 
-def get_statistics(battletag, device='pc', region='us', session=None):
-    return parse_stats_page(get_page(battletag))
+
+def get_statistics(battletag: str, device: str = 'pc', region: str = 'us', session: str = None) -> dict:
+    return parse_stats_page(get_page(assemble_url(battletag, device=device, region=region), session=session))
+
+
+def retrigger(hotkey: str = 'home', linux_warning: bool = False):
+    """
+    Blocks waiting for a hotkey (or enter) to be pressed.
+    :param hotkey: The hotkey/key combination to listen for. See the `keyboard` library for documentation.
+    :param linux_warning: Hotkeys do not work on linux. If this parameter is False, it will send a warning on a Linux machine.
+    :return: Whether the user has been warned at some point.
+    """
+    try:
+        keyboard.wait(hotkey)
+    except ImportError:
+        just_warned = False
+        if not linux_warning:
+            logging.warning("You must be root in order to register hotkeys on linux. Press enter instead to refresh stats")
+            just_warned = True
+        input()
+        return linux_warning or just_warned
+
+linux_warning = False
 
 while True:
     #keyboard.wait('ctrl')
@@ -76,6 +142,5 @@ while True:
     print("{wins} comp wins out of {games} games.".format(wins=stats["competitive_wins"], games=stats["competitive_games"]))
     for hero, minutes in stats["heroes"].items():
         print("{hero}: {time} Minutes Played".format(hero=hero, time=minutes))
-    print("(press return to pull stats again)")
-    input()
+    linux_warning = retrigger(linux_warning=linux_warning)
 
